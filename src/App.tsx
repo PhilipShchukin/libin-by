0785632d61808b04a176538ed2cda91cf7915a82
PhilipShchukin <filippshukin@bk.ru>
2023-08-21 22:29:30 +0300

    prettier install

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { Routes, Route,NavLink } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/HomePage';
 
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavLink to={'/'}> Home</NavLink> 
-      <div>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-        </Routes>
-      </div>
+    <div className='App'>
+      <header className='App-header'>
+        <NavLink to={'/'}> Home</NavLink>
+        <div>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
       </header>
     </div>
   );
