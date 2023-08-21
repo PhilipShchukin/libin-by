15d669c26410145a7379b3384036fca89cc9d081
PhilipShchukin <filippshukin@bk.ru>
2023-08-21 22:14:04 +0300

    eslint commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import React from "react";
 import { Routes, Route,NavLink } from 'react-router-dom';
+
+import './App.css';
 
 import HomePage from './pages/HomePage';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
